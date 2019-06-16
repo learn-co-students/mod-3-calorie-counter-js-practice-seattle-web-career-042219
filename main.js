2fdb20fbd8760357a12eb3cd15ae337030252395
github username <gregm9999@gmail.com>
2019-06-16 10:01:19 -0700

    Done.

--- main.js
+++ main.js
@@ -3,17 +3,23 @@ document.addEventListener("DOMContentLoaded", () => {
   addListeners();
 });
 
-const getAPI = "http://localhost:3000/api/v1/calorie_entries";
+const baseURL = "http://localhost:3000/api/v1/calorie_entries";
 
 function addListeners() {
-  let form = document.getElementById("new-calorie-form");
+  const form = document.getElementById("new-calorie-form");
   form.addEventListener("submit", e => {
     e.preventDefault();
-    postCalorieIntake(e);
+    postEntry(e);
+  });
+  const calBtn = document.getElementById("calculate-button");
+  calBtn.addEventListener("click", e => {
+    e.preventDefault();
+    calculateBMI();
   });
 }
+
 function getCall() {
-  fetch(getAPI)
+  fetch(baseURL)
     .then(res => res.json())
     .then(data => loadInfo(data))
     .catch(err => console.log(err));
@@ -32,46 +38,49 @@ function loadOneDatum(datum) {
   let li = document.createElement("li");
   li.classList.add("calories-list-item");
 
-  let div1 = document.createElement("div");
-  div1.classList.add("uk-grid");
+  let mainDiv1 = document.createElement("div");
+  mainDiv1.classList.add("uk-grid");
 
-  let div2 = document.createElement("div");
-  div2.classList.add("uk-width-1-6");
-  div2.innerHTML = ` <strong>${datum.calorie}</strong>
+  let calorieDiv2 = document.createElement("div");
+  calorieDiv2.classList.add("uk-width-1-6");
+  calorieDiv2.innerHTML = ` <strong>${datum.calorie}</strong>
   <span>kcal</span>`;
 
-  let div3 = document.createElement("div");
-  div3.classList.add("uk-width-4-5");
-  div3.innerHTML = `<em class="uk-text-meta">${datum.note}</em>`;
+  let noteDiv3 = document.createElement("div");
+  noteDiv3.classList.add("uk-width-4-5");
+  noteDiv3.innerHTML = `<em class="uk-text-meta">${datum.note}</em>`;
 
-  let div4 = document.createElement("div");
-  div4.classList.add("list-item-menu");
+  let menuDiv4 = document.createElement("div");
+  menuDiv4.classList.add("list-item-menu");
 
-  let a1 = document.createElement("a");
-  a1.setAttribute("class", "edit-button"),
-    a1.setAttribute("uk-icon", "icon: pencil");
-  a1.setAttribute("uk-toggle", "target: #edit-form-container");
+  let a1Edit = document.createElement("a");
+  a1Edit.setAttribute("class", "edit-button"),
+    a1Edit.setAttribute("uk-icon", "icon: pencil"),
+    a1Edit.setAttribute("uk-toggle", "target: #edit-form-container");
+  a1Edit.addEventListener("click", e => {
+    e.preventDefault();
+    openForm(datum, li);
+  });
 
-  let a2 = document.createElement("a");
-  a2.classList.add("delete-button");
-  a2.setAttribute("uk-icon", "icon: trash");
-  a2.addEventListener("click", e => {
-    e.preventDefault;
+  let a2Delete = document.createElement("a");
+  a2Delete.setAttribute("class", "delete-button");
+  a2Delete.setAttribute("uk-icon", "icon: trash");
+  a2Delete.addEventListener("click", e => {
+    e.preventDefault();
     deleteEntry(datum, li);
   });
 
-  div4.appendChild(a1);
-  div4.appendChild(a2);
-  div1.appendChild(div2);
-  div1.appendChild(div3);
-  li.appendChild(div1);
-  li.appendChild(div4);
+  menuDiv4.appendChild(a1Edit);
+  menuDiv4.appendChild(a2Delete);
+  mainDiv1.appendChild(calorieDiv2);
+  mainDiv1.appendChild(noteDiv3);
+  li.appendChild(mainDiv1);
+  li.appendChild(menuDiv4);
   ulCaloriesList.prepend(li);
 }
 
-function calculateSum(datum) {
-  console.log("datum=", datum);
-  let progressBar = document.getElementsByClassName("uk-progress");
+function calculateSum() {
+  let progressBar = document.getElementById("progress-bar");
   let calTotal = document.getElementsByClassName("uk-width-1-6");
   let sum = 0;
   for (let i = 0; i < calTotal.length; i++) {
@@ -81,17 +90,28 @@ function calculateSum(datum) {
   //     let sum = 0;
   //     sum += element.calorie;
   //   });
-  progressBar[0].max = 6000;
-  progressBar[0].value = sum;
-  console.log("progressBar.value=", progressBar[0].value);
+
+  progressBar.value = sum;
 }
 
-function postCalorieIntake(e) {
+function calculateBMI() {
+  let weight = document.getElementById("weight").value;
+  let height = document.getElementById("height").value;
+  let age = document.getElementById("age").value;
+  let lowerRange = 655 + 4.35 * weight + 4.7 * height - 4.7 * age;
+  let higherRange = 655 + 6.23 * weight + 12.7 * height - 6.8 * age;
+  document.getElementById("lower-bmr-range").innerHTML = lowerRange;
+  document.getElementById("higher-bmr-range").innerHTML = higherRange;
+  let progressBar = document.getElementById("progress-bar");
+  progressBar.max = lowerRange + higherRange / 2;
+}
+
+function postEntry(e) {
   let calories = e.target[0].value;
   let note = e.target[1].value;
   e.target[0].value = "";
   e.target[1].value = "";
-  fetch(getAPI, {
+  fetch(baseURL, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -109,8 +129,42 @@ function postCalorieIntake(e) {
     .catch(err => console.log(err));
 }
 
+function openForm(datum, li) {
+  let editCalories = document.getElementById("edit-calories");
+  let editNotes = document.getElementById("edit-notes");
+  editCalories.value = datum.calorie;
+  editNotes.value = datum.note;
+  const editForm = document.getElementById("edit-calorie-form");
+  editForm.addEventListener("submit", e => {
+    e.preventDefault();
+    editEntry(editCalories, editNotes, datum, li);
+  });
+}
+
+function editEntry(editCalories, editNotes, datum, li) {
+  let calories = editCalories.value;
+  let note = editNotes.value;
+  fetch(baseURL + "/" + datum.id, {
+    method: "PATCH",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    body: JSON.stringify({
+      api_v1_calorie_entry: { calorie: calories, note: note }
+    })
+  })
+    .then(res => res.json())
+    .then(function(response) {
+      loadOneDatum(response);
+      calculateSum(response);
+    })
+    .then(li.remove())
+    .catch(err => console.log(err));
+}
+
 function deleteEntry(datum, li) {
-  fetch(getAPI + "/" + datum.id, {
+  fetch(baseURL + "/" + datum.id, {
     method: "DELETE"
   })
     .then(res => res.json)
